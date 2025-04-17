d2ba68cbd8c4e689c9ccd1940411a888d10f1c82
ZackTanCZ <tan.cz1993@gmail.com>
2025-04-17 17:04:50 +0800

    Add files via upload

---
+++ data_transformation.py
@@ -0,0 +1,137 @@
+import json, re, datetime as dt
+import pandas as pd, numpy as np
+import streamlit as st
+
+
+def addDateTimeColumns(df):
+    '''
+    Transform the date column into DateTime Datatype
+    Add a 'Month', 'Month_num', 'YearMthKey' and 'Year' Column for mapping purposes
+    '''
+    df['date_dt'] = pd.to_datetime(df['Date'], dayfirst = True, 
+                                   yearfirst = True)
+
+    # Add a 'Year' Column with .dt.year -  2024
+    df['Year'] = df['date_dt'].dt.year
+
+    # Add a 'Month_num' column .dt.month - 5
+    df['Month_num'] = df['date_dt'].dt.month
+
+    # Add a 'Month' Column with .dt.month_name() - May
+    df['Month'] = df['date_dt'].dt.month_name()
+
+    # Add a 'YearMthKey' column - YYYYMM
+    df['YearMthKey'] = (df['Year'] * 100) + df['Month_num']
+
+    # Sort the DataFrame by date
+    df.sort_values(by = ["date_dt"], ascending = True)
+       
+    # Reset the index
+    df.reset_index(drop = True, inplace = True)
+
+    return df
+
+def groupByMonth(df):
+    '''
+    Groups the DataFrame by each month
+    Sums the Amount into the respective month
+    Sorted by Month in ascending order
+    '''
+
+    # Perform a grouby to aggregate the total sum of each month
+    grouped_df = df.groupby(by = ['Month', 'Month_num', 'Year', 'YearMthKey'])['Amount'].sum().reset_index()
+
+    # Sort grouped_df in chronological order
+    grouped_df.sort_values('Month_num', inplace = True)
+
+    # Add % of total column
+    grouped_df['% of Total'] = round(((grouped_df['Amount']/(grouped_df['Amount'].sum())) * 100), 2) 
+
+    # reset the index
+    grouped_df.reset_index(drop = True, inplace = True)
+
+    # Cast to the appropriate type
+    grouped_df = grouped_df.astype({'Month':'string', 
+                                    'Month_num':'int',
+                                    'Amount':'float',
+                                    '% of Total':'float'}) 
+
+    # print(grouped_df.dtypes)
+    return grouped_df
+
+def getReviewPeriod(df):
+    """Extracts the earliest and latest Month(MMM) and Year from the DataFrame based on YearMthKey."""
+    minYearMthKey, maxYearMthKey = min(df['YearMthKey']), max(df['YearMthKey'])
+    minRow = df[df['YearMthKey'] == minYearMthKey].iloc[0]
+    maxRow = df[df['YearMthKey'] == maxYearMthKey].iloc[0]
+    return f"{minRow['Month'][:3]} {minRow['Year']}", f"{maxRow['Month'][:3]} {maxRow['Year']}" 
+
+def getMinMaxExpenses(df):
+    '''Extract the Month with the Highest and Lowest Expenses'''
+    minExp, maxExp = min(df['Amount']), max(df['Amount'])
+    minMonth = df[df['Amount'] == minExp]['Month'].tolist()[0]
+    maxMonth = df[df['Amount'] == maxExp]['Month'].tolist()[0]
+    return minExp, minMonth, maxExp, maxMonth
+
+def categoriseDelta(num):
+    if num > 0:
+        return "🟥"
+    elif (num < 0):
+        return "🟩"
+    else: 
+        return "⬜"
+
+def highlight_row(row, highest_delta_index):
+    """Highlights the row with the highest 'delta' value."""
+    if row.name == highest_delta_index:
+        return ['background-color: #FF0800'] * len(row)
+    return [''] * len(row)
+
+def addLaggedOnePeriod(df):
+    '''Adds one column of values from the period before'''
+    # df.shift() to shift values down by one row e.g. value of Jan will appear in Feb
+    df['ty-1'] = df['Amount'].shift(periods = 1, axis = 0)
+
+    base = np.where(df['ty-1'].isna(), df['Amount'],df['ty-1'])
+
+    df['delta'] = np.where(df['ty-1'].isna(), 0, round((((df['Amount'] - base) / base) * 100),2))
+
+    df['change'] = df['delta'].apply(categoriseDelta)
+
+    highest_delta_index = df['delta'].idxmax()
+
+    styled_df = df.style.apply(highlight_row, axis=1, highest_delta_index=highest_delta_index).format(precision=2)
+
+    return styled_df
+
+def groupByCategory(df):
+    '''
+    Group the DataFrame by the 'Category' Column
+    '''
+    
+    # Perform a grouby to aggregate the total sum of each month
+    grouped_df = df.groupby(by = ['Category'])['Amount'].sum().reset_index()
+
+    # Sort grouped_df in chronological order
+    grouped_df.sort_values('Amount', ascending = False, inplace = True)
+
+    # reset the index
+    grouped_df.reset_index(drop = True, inplace = True)
+
+    # Cast to the appropriate type
+    grouped_df = grouped_df.astype({'Category':'string',
+                                    'Amount':'float'}) 
+
+    # print(grouped_df.dtypes)
+    return grouped_df
+
+def getFilteredDF(sel_month, df):
+    if (sel_month == "All Months"):
+        filterDF = groupByCategory(df)
+    else:
+        filterDF = groupByCategory(df[df['Month'] == sel_month])
+
+    total_val = filterDF["Amount"].sum()
+    filterDF["% of Total(%)"] = round((filterDF["Amount"]/total_val) * 100, 2)
+
+    return filterDF
---
+++ main.py
@@ -0,0 +1,117 @@
+import json, re, datetime as dt
+import pandas as pd, numpy as np
+import altair as alt
+import streamlit as st
+import data_ingestion as di
+import data_transformation as dx
+
+
+# st.write('Hello world!')
+# # use streamlit run main.py to run
+
+# # 1. Data Ingestion
+# # Only to refresh my datasource from Google Sheet
+# # 1a. Ingest Data from Google Sheets and load into the 'dataDF' DataFrame
+# dataDF = di.getDataFromGoogleSheets()
+
+# # 1b. Store 'DataDF' as another Google Sheet
+# di.compileDataToGoogleSheet(dataDF, 'expensesgsheet')
+
+# # 1c. Retrieve all records from Google Sheet, load into a DataFrame and save as a .csv file
+# df_name = di.getDataForDataFrame()
+
+# 2. Data Transformation
+# 2a. Load the csv as a DataFrame
+dataDF = pd.read_csv('expenses_df.csv')
+
+# 2b. Add a DateTime Column
+dataDF = dx.addDateTimeColumns(dataDF)
+
+# 2c. Perform a GroupBy to find the Total sum by Month
+groupByMonthDF = dx.groupByMonth(dataDF)
+avg_value =  round(groupByMonthDF['Amount'].mean(),2)
+groupByMonthDF['avg'] = avg_value
+
+# # 2d. Perform a GroupBy to find the Total sum by Category
+# groupbyCategory = dx.groupByCategory(dataDF)
+
+
+# 3. Code for the Dashboard UI
+# 3a. Set the page options to change the layout to wide
+st.set_page_config(layout="wide")
+
+# 3b. Creates three columns
+p1, p2, p3   = st.columns([0.2, 0.5, 0.3], border = True)
+
+# 4. Setup each columns with their respective elements
+# 4a. Set the first column (p1) to display descriptive statistics
+with p1:
+
+    minYearMth, maxYearMth = dx.getReviewPeriod(groupByMonthDF)
+
+    st.header("Review Period\n{} \nTo {}".format(minYearMth,maxYearMth))
+    st.text("Total Expenses\n${}".format(groupByMonthDF['Amount'].sum()))
+    st.text("Average Monthly Expenses\n${}".format(avg_value))
+
+    # c declare the properties of the chart
+    line_chart = alt.Chart(groupByMonthDF).mark_line(point = True).encode(
+        alt.X("Month_num").axis(labels = False, title = "", grid = False),
+        alt.Y("Amount:Q").axis(labels = False, title = "", grid = False), 
+        tooltip=["Month", "Amount"]).properties(width = 100, height = 200)
+    
+    # Create the average line
+    avg_line = alt.Chart(groupByMonthDF).mark_rule(color='white', strokeDash=[5, 5]).encode(
+    alt.Y("avg:Q"),
+    tooltip = ["Month", "avg"]).properties(width = 100, height = 200)
+    
+    # Create the text label at the left of the average line
+    text = alt.Chart(pd.DataFrame({'y': [avg_value], 'text': [f'Avg: ${avg_value:.2f}'], 'x': [0.5]})).mark_text(
+        align='left', dx=5, dy=-5, color='white').encode(y='y:Q', text='text:N', x = 'x:Q')
+
+    # cc = ()#.configure_view(strokeWidth=1,stroke = 'grey')
+
+    cc = alt.layer(line_chart, avg_line, text).properties(title="Monthly Expenses").configure_point(size = 75)
+
+    st.altair_chart(cc)
+
+    minExp, minMonth, maxExp, maxMonth = dx.getMinMaxExpenses(groupByMonthDF)
+    st.text("Lowest Month - {}\n${}".format(minMonth, minExp ))
+    st.text("Highest Month - {}\n${}".format(maxMonth, maxExp))
+
+
+# 4b. Second Columns (p2) to display the DataFrame
+groupByMonthDFlagged = dx.addLaggedOnePeriod(groupByMonthDF)
+with p2:
+    st.dataframe(groupByMonthDFlagged,
+                 height = 458, 
+                 hide_index = True, 
+                 column_order = ("Month","Amount", "% of Total","ty-1","delta","change"),
+                 column_config= {"Amount": "Total Expenses ($)",
+                                 "ty-1": "Prev. Month Expense ($)",
+                                 "delta": "Δ% from Prev. Month",
+                                 "change": ""} )
+
+# 4c. Third Column (p3) to display Charts
+
+with p3:
+    # Performs a GroupBy with 'Category' Column
+    sel_month = st.selectbox(label = "**Choose a Month**", options = ["All Months"] + groupByMonthDF['Month'].to_list())
+    filterDF = dx.getFilteredDF(sel_month, dataDF)
+
+    # Setting up the bar chart
+    bar_chart = alt.Chart(filterDF.head(3), title = "Top 3 Categories - {}".format(sel_month)).mark_bar().encode(
+        alt.X("Amount:Q").axis(labels = True, title = "Amount($)"),
+        alt.Y("Category:N", sort = alt.EncodingSortField(field = "Amount", order = "descending")).axis(labels = True, title = ""),
+        tooltip = ["Category","Amount"]
+    ).properties(width = 100, height = 250)
+
+    st.altair_chart(bar_chart)
+
+    # Setting another DataFrame to display more details
+    # Reuse filterDF
+    st.text("Category Breakdown")
+    st.dataframe(filterDF.round(2),
+                 hide_index = True,
+                 column_config= {"Amount": "Total ($)"}                 
+                 )
+
